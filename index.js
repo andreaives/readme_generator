efd86fffc461f56415242ed2f2c76def36dc8446
andreaives <andreasue3@gmail.com>
2020-10-14 17:20:06 -0600

    added write to file function

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 //require files needed
 var fs = require("fs")
 var inquirer = require("inquirer")
-var markdown = require("generate_markdown.js")
+var markdown = require("./generate_markdown")
+// const { listenerCount } = require("process")
+const path = require("path")
 
-//need project title this is displayed as the title of the README
 
 
 //description, installation instructions, usage information, contribution guidelines, and test instructions
 //added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
 const questions = [
  {
+  //grabbing a name of the project from a user
+  type: "input",
+  message: "Give your README a name.",
+  name: 'readmeName'
+ },{
+  //getting a description of the project from a user
   type: "input",
   message: "Describe you project.",
   name: 'description'
  },
  {
+  //getting the installation section information
   type: "input",
   message: "Installation instructions:",
   name: 'installation'
  },
  {
+  //getting the section about usage information
   type: "input",
   message: "Usage information:",
   name: 'usage'
@@ -29,25 +38,34 @@ const questions = [
   message: "Tests:",
   name: 'tests'
  },
-
+ {
+  //list of liscense options
+  type: "list",
+  message: "Choose the license used:",
+  name: "license",
+  choices: ["IBM","ISC License","MIT", "Mozilla","none"]
+ }
 ]
-
-// array of questions for user
-
 
 
 
 // function to write README file
 function writeToFile(fileName, data) {
- return fs.writeFileSync(fileName, data)
+ return fs.writeFileSync(path.join(process.cwd(),fileName),data)
+ //(fileName, data)
 }
-
 // function to initialize program
 function init() {
- //take in questions
+
+ //calling the questiosn to prompt user 
+ inquirer.prompt(questions).then(response =>{
  //pass in the response
- //generate response as data
- //call write to file function here 
+  //generate response as data
+  //call write to file function here 
+  writeToFile("practiceReadme.md", markdown({...response}))
+
+
+})
 
 }
 
