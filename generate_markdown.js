426773a01aed8469a7032a69cf3fbc15f39aa23a
andreaives <andreasue3@gmail.com>
2020-10-16 14:10:38 -0600

    created liscense badge function

--- generate_markdown.js
+++ generate_markdown.js
@@ -1,3 +1,11 @@
+
+
+function licenseBadge(userLiscense){
+ return `![GitHub license] (https://img.shields.io/badge/License-${userLiscense}-blue.svg)`
+}
+
+
+
 function generateMarkdown(data) {
  //add the indexfile to the exports
  return `# ${data.title}
