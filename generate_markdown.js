7faad4de6eb3260ed4586678af731ea54e9e6f33
andreaives <andreasue3@gmail.com>
2020-10-16 14:21:50 -0600

    Finished Table of Contents

--- generate_markdown.js
+++ generate_markdown.js
@@ -1,15 +1,28 @@
 
-
+//pulling liscense badge 
 function licenseBadge(userLiscense){
  return `![GitHub license] (https://img.shields.io/badge/License-${userLiscense}-blue.svg)`
 }
-
-
-
+//marking down reaad me with user information
 function generateMarkdown(data) {
- //add the indexfile to the exports
- return `# ${data.title}
- return # ${data.title}`;
+ return `${renderLicenseBadge(data.license)}
+ # ${data.readmeName}
+
+ ## Table of Contents
+ * [Description](#description)
+ * [Installation](#installation)
+ * [Contributing](#contibuting)
+ * [Tests](#tests)
+ *[Questions](#questions)
+ 
+ ## Description
+ ${data.description}
+ 
+ 
+ 
+ 
+ `
+
 }
 
 module.exports = generateMarkdown;
